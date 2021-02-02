abd7b78c34e47dcf4cc5bacda1fae91955d7314f
raju-anabathula <raju.anabathula@gmail.com>
2021-02-02 14:44:55 +0530

    Update answer1.cpp
    Corrected spelling

--- answer1.cpp
+++ answer1.cpp
@@ -3,23 +3,23 @@
 */
 #include <iostream>
 
-bool isPalindrom(const std::string & str) {
+bool isPalindrome(const std::string & str) {
     unsigned int i = 0;
     unsigned int j = str.length() - 1;
     while(i < j) {
         if (::tolower(str[i++]) != ::tolower(str[j--])) {
-            std::cout << str << " is not palindrom" << std::endl;
+            std::cout << str << " is not palindrome" << std::endl;
             return false;
         }
     }
-    std::cout << str << " is palindrom" << std::endl;
+    std::cout << str << " is palindrome" << std::endl;
 }
 
 int main()
 {
-    isPalindrom("1881");
-    isPalindrom("Dad");
-    isPalindrom("Step on no pets");
+    isPalindrome("1881");
+    isPalindrome("Dad");
+    isPalindrome("Step on no pets");
     
     return 0;
 }
